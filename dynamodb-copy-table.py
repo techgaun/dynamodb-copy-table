7f6b33c784879d2e5b32958625e4b801eec3758d
c-py <craig@assignar.com>
2020-04-02 10:10:19 +1100

    Update for python 3

--- dynamodb-copy-table.py
+++ dynamodb-copy-table.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 from boto.dynamodb2.exceptions import ValidationException
 from boto.dynamodb2.fields import HashKey, RangeKey
 from boto.dynamodb2.layer1 import DynamoDBConnection
@@ -9,8 +9,7 @@ import sys
 import os
 
 if len(sys.argv) != 3:
-    print 'Usage: %s <source_table_name>' \
-        ' <destination_table_name>' % sys.argv[0]
+    print("Usage: %s <source_table_name> <destination_table_name>"% sys.argv[0])
     sys.exit(1)
 
 src_table = sys.argv[1]
@@ -28,10 +27,10 @@ try:
     logs = Table(src_table, connection=ddbc)
     table_struct = logs.describe()
 except JSONResponseError:
-    print "Table %s does not exist" % src_table
+    print("Table %s does not exist" % src_table) 
     sys.exit(1)
 
-print '*** Reading key schema from %s table' % src_table
+print("*** Reading key schema from %s table" % src_table)
 src = ddbc.describe_table(src_table)['Table']
 hash_key = ''
 range_key = ''
@@ -47,15 +46,18 @@ for schema in src['KeySchema']:
 table_struct = None
 try:
     new_logs = Table(dst_table,
-                     connection=ddbc,
-                     schema=[HashKey(hash_key),
-                             RangeKey(range_key),
-                             ]
-                     )
+                    connection=ddbc,
+                    schema=[HashKey(hash_key),
+                            RangeKey(range_key),
+                            ]
+                    )
 
     table_struct = new_logs.describe()
-    print 'Table %s already exists' % dst_table
-    sys.exit(0)
+    if 'DISABLE_CREATION' in os.environ:
+        print("Creation of new table is disabled. Skipping...")
+    else:
+        print("Table %s already exists" % dst_table)
+        sys.exit(0)
 except JSONResponseError:
     schema = [HashKey(hash_key)]
     if range_key != '':
@@ -64,13 +66,14 @@ except JSONResponseError:
                             connection=ddbc,
                             schema=schema,
                             )
-    print '*** Waiting for the new table %s to become active' % dst_table
+    print("*** Waiting for the new table %s to become active" % dst_table)
     sleep(5)
     while ddbc.describe_table(dst_table)['Table']['TableStatus'] != 'ACTIVE':
         sleep(3)
+    
 
 if 'DISABLE_DATACOPY' in os.environ:
-    print 'Copying of data from source table is disabled. Exiting...'
+    print("Copying of data from source table is disabled. Exiting...")
     sys.exit(0)
 
 # 3. Add the items
@@ -87,8 +90,8 @@ for item in logs.scan():
         new_logs.use_boolean()
         new_logs.put_item(new_item, overwrite=True)
     except ValidationException:
-        print dst_table, new_item
+        print(dst_table, new_item)
     except JSONResponseError:
-        print ddbc.describe_table(dst_table)['Table']['TableStatus']
+        print(ddbc.describe_table(dst_table)['Table']['TableStatus'])
 
-print 'We are done. Exiting...'
+print("We are done. Exiting...")
