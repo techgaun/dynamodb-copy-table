ed36498440119d41ed40008e9b7bf8691745a618
Peter Belm <126499052+Detectronic-PB@users.noreply.github.com>
2025-07-04 13:45:17 +0100

    Upgraded to boto3 Added cross-region support

--- dynamodb-copy-table.py
+++ dynamodb-copy-table.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
-from boto.dynamodb2.exceptions import ValidationException
-from boto.dynamodb2.fields import HashKey, RangeKey
-from boto.dynamodb2.layer1 import DynamoDBConnection
-from boto.dynamodb2.table import Table
-from boto.exception import JSONResponseError
+import boto3
+from botocore.exceptions import ClientError
 from time import sleep
 import sys
 import os
 
 if len(sys.argv) != 3:
-    print("Usage: %s <source_table_name> <destination_table_name>"% sys.argv[0])
+    print("Usage: %s <source_table_name> <destination_table_name>" % sys.argv[0])
     sys.exit(1)
 
 src_table = sys.argv[1]
 dst_table = sys.argv[2]
-region = os.getenv('AWS_DEFAULT_REGION', 'us-west-2')
+default_region = os.getenv('AWS_DEFAULT_REGION', 'us-west-2')
+src_region = os.getenv('AWS_SRC_REGION', default_region)
+dst_region = os.getenv('AWS_dst_REGION', default_region)
 
-# host = 'dynamodb.%s.amazonaws.com' % region
-# ddbc = DynamoDBConnection(is_secure=False, region=region, host=host)
-DynamoDBConnection.DefaultRegionName = region
-ddbc = DynamoDBConnection()
+src_resource = boto3.resource('dynamodb', region_name=src_region)
+dst_resource = boto3.resource('dynamodb', region_name=dst_region)
+dst_client = boto3.client('dynamodb', region_name=dst_region)
 
 # 1. Read and copy the target table to be copied
-table_struct = None
 try:
-    logs = Table(src_table, connection=ddbc)
-    table_struct = logs.describe()
-except JSONResponseError:
-    print("Table %s does not exist" % src_table) 
+    logs = src_resource.Table(src_table)
+    src_desc = logs.table_status  # Will raise if table doesn't exist
+except Exception:
+    print("Table %s does not exist" % src_table)
     sys.exit(1)
 
 print("*** Reading key schema from %s table" % src_table)
-src = ddbc.describe_table(src_table)['Table']
+src_table_desc = src_resource.meta.client.describe_table(TableName=src_table)['Table']
+key_schema = src_table_desc['KeySchema']
+attribute_definitions = src_table_desc['AttributeDefinitions']
+
 hash_key = ''
 range_key = ''
-for schema in src['KeySchema']:
+for schema in key_schema:
     attr_name = schema['AttributeName']
     key_type = schema['KeyType']
     if key_type == 'HASH':
@@ -43,55 +43,70 @@ for schema in src['KeySchema']:
         range_key = attr_name
 
 # 2. Create the new table
-table_struct = None
 try:
-    new_logs = Table(dst_table,
-                    connection=ddbc,
-                    schema=[HashKey(hash_key),
-                            RangeKey(range_key),
-                            ]
-                    )
-
-    table_struct = new_logs.describe()
+    dst_table_obj = dst_resource.Table(dst_table)
+    dst_table_obj.load()
     if 'DISABLE_CREATION' in os.environ:
         print("Creation of new table is disabled. Skipping...")
     else:
         print("Table %s already exists" % dst_table)
         sys.exit(0)
-except JSONResponseError:
-    schema = [HashKey(hash_key)]
-    if range_key != '':
-        schema.append(RangeKey(range_key))
-    new_logs = Table.create(dst_table,
-                            connection=ddbc,
-                            schema=schema,
-                            )
-    print("*** Waiting for the new table %s to become active" % dst_table)
-    sleep(5)
-    while ddbc.describe_table(dst_table)['Table']['TableStatus'] != 'ACTIVE':
-        sleep(3)
-    
+except ClientError as e:
+    if e.response['Error']['Code'] == 'ResourceNotFoundException':
+        if 'DISABLE_CREATION' in os.environ:
+            print("Creation of new table is disabled. Skipping...")
+            sys.exit(0)
+        # Use same key schema and attribute definitions as source
+        params = {
+            'TableName': dst_table,
+            'KeySchema': key_schema,
+            'AttributeDefinitions': attribute_definitions,
+            'BillingMode': src_table_desc.get('BillingModeSummary', {}).get('BillingMode', 'PAY_PER_REQUEST')
+        }
+        if 'ProvisionedThroughput' in src_table_desc:
+            params['ProvisionedThroughput'] = {
+                'ReadCapacityUnits': src_table_desc['ProvisionedThroughput']['ReadCapacityUnits'],
+                'WriteCapacityUnits': src_table_desc['ProvisionedThroughput']['WriteCapacityUnits']
+            }
+        dst_client.create_table(**params)
+        print("*** Waiting for the new table %s to become active" % dst_table)
+        waiter = dst_client.get_waiter('table_exists')
+        waiter.wait(TableName=dst_table)
+        # Wait until ACTIVE
+        while dst_resource.Table(dst_table).table_status != 'ACTIVE':
+            sleep(3)
+        dst_table_obj = dst_resource.Table(dst_table)
+    else:
+        raise
 
 if 'DISABLE_DATACOPY' in os.environ:
     print("Copying of data from source table is disabled. Exiting...")
     sys.exit(0)
 
 # 3. Add the items
-for item in logs.scan():
-    new_item = {}
-    new_item[hash_key] = item[hash_key]
-    if range_key != '':
-        new_item[range_key] = item[range_key]
-    for f in item.keys():
-        if f in [hash_key, range_key]:
-            continue
-        new_item[f] = item[f]
-    try:
-        new_logs.use_boolean()
-        new_logs.put_item(new_item, overwrite=True)
-    except ValidationException:
-        print(dst_table, new_item)
-    except JSONResponseError:
-        print(ddbc.describe_table(dst_table)['Table']['TableStatus'])
+scan_kwargs = {}
+done = False
+start_key = None
+
+while not done:
+    if start_key:
+        scan_kwargs['ExclusiveStartKey'] = start_key
+    response = logs.scan(**scan_kwargs)
+    items = response.get('Items', [])
+    for item in items:
+        new_item = {}
+        new_item[hash_key] = item[hash_key]
+        if range_key:
+            new_item[range_key] = item[range_key]
+        for f in item.keys():
+            if f in [hash_key, range_key]:
+                continue
+            new_item[f] = item[f]
+        try:
+            dst_table_obj.put_item(Item=new_item)
+        except ClientError as e:
+            print(dst_table, new_item, e)
+    start_key = response.get('LastEvaluatedKey', None)
+    done = start_key is None
 
 print("We are done. Exiting...")
